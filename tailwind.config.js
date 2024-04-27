f81340a7337484e114fab7040b6619927e10c440
serhiydemchuk <serhiy.demczuk@gmail.com>
2024-04-27 16:10:05 +0300

    add submit form and state for message

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
+const { blackA, violet, mauve } = require("@radix-ui/colors");
+
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    theme: {
+      extend: {
+        colors: {
+          ...blackA,
+          ...violet,
+          ...mauve,
+        },
+      },
+    },
   },
   plugins: [],
 };
